d1a89f78065be6f1874b17778b97ddf9b1c00793
Denis Vasilev <n1hron@yandex.com>
2025-05-16 22:06:01 +0300

    Add more button states

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -3,13 +3,14 @@ import clsx from 'clsx';
 import styles from './style.module.scss';
 
 type ButtonProps = {
-  color?: 'red';
+  color?: 'red' | 'green';
   ghost?: boolean;
 } & React.ComponentPropsWithRef<'button'>;
 
 function Button({ className, color, ghost, ...props }: ButtonProps) {
   const cn = clsx(
     styles.button,
+    color && styles.colored,
     color && styles[color],
     ghost && styles.ghost,
     className
